4969077a5679c1fcf45870f6eace12adcf0d25dc
Wilker Chaves <chaveswill8@gmail.com>
2022-06-16 21:19:26 -0300

    Dev Profile created

---
+++ src/main/java/OS/config/DevConfig.java
@@ -0,0 +1,52 @@
+package OS.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import OS.model.Client;
+import OS.model.OrderService;
+import OS.model.Technician;
+import OS.model.enums.OrderPriority;
+import OS.model.enums.OrderStatus;
+import OS.services.ClientService;
+import OS.services.OSService;
+import OS.services.TechnicianService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private OSService service;
+
+	@Autowired
+	private ClientService clientService;
+
+	@Autowired
+	private TechnicianService technicianService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String ddl;
+	@Bean
+	public boolean init() {
+	
+		if(ddl.equals("create")) {
+			Technician t1 = new Technician(null, "André Silva", "861.414.100-90", "93 88888888");
+			Client c1 = new Client(null, "Maria Eduarda", "152.197.520-57", "93 87875454");
+			Client c2 = new Client(null, "João Silva", "321.946.910-81", "93 85462123");
+			OrderService os = new OrderService(null, OrderPriority.BAIXA, "", OrderStatus.EM_ANDAMENTO, t1, c1);
+			t1.getOrderServices().add(os);
+			c1.getOrderServices().add(os);
+
+			technicianService.save(t1);
+			clientService.save(c1);
+			clientService.save(c2);
+			service.save(os);
+		}
+		return false;
+
+	}
+}
--- src/main/java/OS/config/TestConfig.java
+++ src/main/java/OS/config/TestConfig.java
@@ -31,12 +31,14 @@ public class TestConfig {
 	public void init() {
 		Technician t1 = new Technician(null, "André Silva", "861.414.100-90", "93 88888888");
 		Client c1 = new Client(null, "Maria Eduarda", "152.197.520-57", "93 87875454");
+		Client c2 = new Client(null, "João Silva", "321.946.910-81", "93 85462123");
 		OrderService os = new OrderService(null, OrderPriority.BAIXA, "", OrderStatus.EM_ANDAMENTO, t1, c1);
 		t1.getOrderServices().add(os);
 		c1.getOrderServices().add(os);
 
 		technicianService.save(t1);
 		clientService.save(c1);
+		clientService.save(c2);
 		service.save(os);
 
 	}
