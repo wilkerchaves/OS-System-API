4123a0d902a541c3fa940ebd75b49b7205e87a3b
Wilker Chaves <chaveswill8@gmail.com>
2022-06-16 21:06:14 -0300

    Test profile created

--- src/main/java/OS/OsApplication.java
+++ src/main/java/OS/OsApplication.java
@@ -1,49 +1,16 @@
 package OS;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import OS.model.Client;
-import OS.model.OrderService;
-import OS.model.Technician;
-import OS.model.enums.OrderPriority;
-import OS.model.enums.OrderStatus;
-import OS.services.ClientService;
-import OS.services.OSService;
-import OS.services.TechnicianService;
-
 @SpringBootApplication
-public class OsApplication implements CommandLineRunner {
+public class OsApplication{
 	
-	@Autowired
-	private OSService service;
-	
-	@Autowired
-	private ClientService clientService;
-	
-	@Autowired
-	private TechnicianService technicianService;
 	
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Technician t1 = new Technician(null, "André Silva", "861.414.100-90", "93 88888888");
-		Client c1 = new Client(null, "Maria Eduarda", "152.197.520-57", "93 87875454");
-		OrderService os = new OrderService(null, OrderPriority.BAIXA, "", OrderStatus.EM_ANDAMENTO, t1, c1);
-		t1.getOrderServices().add(os);
-		c1.getOrderServices().add(os);
-		
 	
-		technicianService.save(t1);
-		clientService.save(c1);
-		service.save(os);
-		
-	}
-
 }
---
+++ src/main/java/OS/config/TestConfig.java
@@ -0,0 +1,43 @@
+package OS.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import OS.model.Client;
+import OS.model.OrderService;
+import OS.model.Technician;
+import OS.model.enums.OrderPriority;
+import OS.model.enums.OrderStatus;
+import OS.services.ClientService;
+import OS.services.OSService;
+import OS.services.TechnicianService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private OSService service;
+
+	@Autowired
+	private ClientService clientService;
+
+	@Autowired
+	private TechnicianService technicianService;
+
+	@Bean
+	public void init() {
+		Technician t1 = new Technician(null, "André Silva", "861.414.100-90", "93 88888888");
+		Client c1 = new Client(null, "Maria Eduarda", "152.197.520-57", "93 87875454");
+		OrderService os = new OrderService(null, OrderPriority.BAIXA, "", OrderStatus.EM_ANDAMENTO, t1, c1);
+		t1.getOrderServices().add(os);
+		c1.getOrderServices().add(os);
+
+		technicianService.save(t1);
+		clientService.save(c1);
+		service.save(os);
+
+	}
+}
