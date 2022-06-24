6fc0ce642003624737aa1ce7506d23968a680a3e
Wilker Chaves <chaveswill8@gmail.com>
2022-06-24 01:04:19 -0300

    CorsConfigure

--- src/main/java/OS/OsApplication.java
+++ src/main/java/OS/OsApplication.java
@@ -2,15 +2,24 @@ package OS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class OsApplication{
-	
-	
+public class OsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsApplication.class, args);
 	}
 
-	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/technicians").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
