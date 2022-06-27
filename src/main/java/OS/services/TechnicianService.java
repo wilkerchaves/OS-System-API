4c72ce3bc0e7e6dbd78bd711cbbe3d946edae214
Wilker Chaves <chaveswill8@gmail.com>
2022-06-27 16:01:15 -0300

    Update TechnicianService.java

--- src/main/java/OS/services/TechnicianService.java
+++ src/main/java/OS/services/TechnicianService.java
@@ -45,6 +45,7 @@ public class TechnicianService {
 	public Technician update(Integer id, @Valid TechnicianDTO dto) {
 		Technician oldObj = getById(id);
 		if (getByCPF(dto) != null && getByCPF(dto).getId() != id) {
+			System.out.println(getByCPF(dto)+"---"+getByCPF(dto).getId() != id);
 			throw new DataIntegrityViolationException("Erro: novo CPF é diferente do registrado!"+getByCPF(dto));
 		}
 		oldObj.setName(dto.getName());
